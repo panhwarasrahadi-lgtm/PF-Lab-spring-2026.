503a6cc33c33eb1f269ab0f641124afed2d24563
panhwarasrahadi-lgtm <panhwarasrahadi@gmail.com>
2026-02-13 21:48:08 +0500

    Create task 1 lab 4.c

---
+++ task 1 lab 4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int percentage;
+		scanf("%d",&percentage);
+	if (percentage>= 75){
+		printf("selected for tornament");
+		}
+		else{
+			printf("not selected");
+	}
+}
