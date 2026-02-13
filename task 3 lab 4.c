90f345634e2be71cd484057a175bbbc7c7d8c323
panhwarasrahadi-lgtm <panhwarasrahadi@gmail.com>
2026-02-13 21:50:32 +0500

    Create task 3 lab 4.c

---
+++ task 3 lab 4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	int balance;
+		scanf("%d",&balance);
+	if (balance >0){
+		printf("positive balance");
+		
+		}
+		else if (balance<0){
+	    	printf("overdrawn");
+		}
+		else{
+			printf("zero Balance");
+		}
+		
+	}
+
