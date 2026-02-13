bedd0c483c9a4a54c7b239d13b7056e34a8819bf
panhwarasrahadi-lgtm <panhwarasrahadi@gmail.com>
2026-02-13 21:48:48 +0500

    Create task 2 lab 4.c

---
+++ task 2 lab 4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int Data;
+		scanf("%d",&Data);
+	if (Data<= 50){
+		printf("Basic user");
+		
+		}
+		else if (Data>51&&Data<150){
+	    	printf("standard user");
+		}
+		else{
+			printf("Heavy User");
+		}
+		
+	}
