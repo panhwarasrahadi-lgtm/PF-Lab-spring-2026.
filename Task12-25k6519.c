d401748381630b757df9c4c587c146f773356a66
panhwarasrahadi-lgtm <panhwarasrahadi@gmail.com>
2026-03-01 21:47:48 +0500

    Update Task12-lab 6.c

--- Task12-25k6519.c
+++ Task12-25k6519.c
@@ -1,12 +1,4 @@
-// 12. Bus Seat Reservation Monitoring System (While Loop – Conditional Termination & Logical Control)
-// A bus has 40 seats available. Passengers arrive one by one to reserve a seat.
-// The system should:
-// •	Allow seat booking until either:
-// o	All 40 seats are booked, OR
-// o	The operator enters 0 to stop reservations early.
-// •	After each booking, display the number of remaining seats.
-// •	Prevent booking if seats are already full.
-// Write a C program using a while loop to manage this reservation process and ensure proper condition control.
+
 #include <stdio.h>
 
 int main() {
@@ -19,18 +11,18 @@ int main() {
         scanf("%d", &choice);
 
         if (choice == 0) {
-            break; // Exit the loop if the operator chooses to stop reservations
-        }
+            break; }
 
         if (bookedSeats < totalSeats) {
             bookedSeats++;
             printf("Seat booked successfully. Remaining seats: %d\n", totalSeats - bookedSeats);
         } else {
             printf("All seats are booked. No more reservations can be made.\n");
-            break; // Exit the loop if all seats are booked
+            break; 
         }
     }
 
     printf("Reservation process ended. Total booked seats: %d\n", bookedSeats);
     return 0;
+
 }
